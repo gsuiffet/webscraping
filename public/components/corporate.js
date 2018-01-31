9093acb048e98664ecd45ff3031932194e9b7c7a
suiffet guillaume <suiffet.g@gmail.com>
2018-01-31 22:39:18 +0100

    display loader when scraping

--- public/components/corporate.js
+++ public/components/corporate.js
@@ -3,9 +3,20 @@ var React = require('react');
 class Corporate extends React.Component {
     constructor() {
         super();
-        this.state = { name: "Corporate name", employees: "number of employees", logo: null, corporate:""};
+        this.state = {
+            name: "Corporate name",
+            employees: "number of employees",
+            logo: null,
+            corporate: "",
+            loader: false
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    showLoader () {
+        this.setState({
+            loader: true
+        });
     }
     handleChange(event) {
         this.setState({corporate: event.target.value});
@@ -23,6 +34,9 @@ class Corporate extends React.Component {
         }).then(function(results) {
             console.log("results",results);
             that.setState({
+                loader: false
+            });
+            that.setState({
                 name: results.name,
                 employees: results.employees,
                 logo: document.getElementById("logoCorporate").src = 'logo'+that.state.corporate+'.png'
@@ -31,6 +45,26 @@ class Corporate extends React.Component {
         event.preventDefault();
     }
     render() {
+        var corporate = "";
+        if (this.state.loader != false) {
+            corporate =
+                <div className="mui-col-md-12 mui--text-center">
+                    <div className="loader"></div>
+                </div>
+        } else {
+            corporate =
+            <div className="mui-col-md-12 mui--text-center">
+                <div className="mui-col-xs-4">
+                    <img src="../src/img/logo.png" id="logoCorporate" alt="logo corporate"/>
+                </div>
+                <div className="mui-col-xs-4" id="infoName">
+                    <p> {this.state.name}</p>
+                </div>
+                <div className="mui-col-xs-4" id="infoEmployees">
+                    <p> {this.state.employees}</p>
+                </div>
+            </div>
+        }
         return (
             <div id="content-wrapper">
                 <div className="mui-container-fluid">
@@ -39,26 +73,16 @@ class Corporate extends React.Component {
                             <div className="mui--appbar-height"></div>
                             <h1>Corporate</h1>
                             <p>Enter the name of one corporate on Viadeo.</p>
-                            <p style={{color:"#777", fontStyle:"italic"}}>Exemple: <a title="coca" href="http://fr.viadeo.com/fr/company/renault" target="blank">renault</a>, <a title="coca" href="http://fr.viadeo.com/fr/company/coca-cola-entreprise" target="blank">coca-cola-entreprise</a>, <a title="coca" href="http://fr.viadeo.com/fr/company/orangina-schweppes" target="blank">orangina-schweppes</a>...</p>
+                            <p style={{color:"#777", fontStyle:"italic"}}>Exemple: <a title="renault" href="http://fr.viadeo.com/fr/company/renault" target="blank">renault</a>, <a title="coca" href="http://fr.viadeo.com/fr/company/coca-cola-entreprise" target="blank">coca-cola-entreprise</a>, <a title="orangina" href="http://fr.viadeo.com/fr/company/orangina-schweppes" target="blank">orangina-schweppes</a>...</p>
                                 <form onSubmit={this.handleSubmit}>
                                     <div className="form">
                                         <label>Corporate name</label>
                                         <input type="text" id="corporate" name="corporate" placeholder="renault" value={this.state.corporate} onChange={this.handleChange}/>
-                                        <input type="submit" className="mui-btn mui-btn--primary" id="valider" value="Find"/>
+                                        <input type="submit" className="mui-btn mui-btn--primary" id="valider" value="Find" onClick={this.showLoader.bind(this)}/>
                                     </div>
                                 </form>
                             <div className="mui-row">
-                                <div className="mui-col-md-12 mui--text-center">
-                                    <div className="mui-col-xs-4">
-                                        <img src="../src/img/logo.png" id="logoCorporate" alt="logo corporate"/>
-                                    </div>
-                                    <div className="mui-col-xs-4" id="infoName">
-                                        <p> {this.state.name}</p>
-                                    </div>
-                                    <div className="mui-col-xs-4" id="infoEmployees">
-                                        <p> {this.state.employees}</p>
-                                    </div>
-                                </div>
+                                {corporate}
                             </div>
                         </div>
                     </div>
