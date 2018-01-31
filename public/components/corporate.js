0494783e3ee82dcc82244e3a35711fd1cfa337a7
suiffet guillaume <suiffet.g@gmail.com>
2018-01-31 18:29:24 +0100

    display data scraped from Viadeo

--- public/components/corporate.js
+++ public/components/corporate.js
@@ -3,6 +3,32 @@ var React = require('react');
 class Corporate extends React.Component {
     constructor() {
         super();
+        this.state = { name: "", employees: "", logo: null, corporate:""};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({corporate: event.target.value});
+    }
+    handleSubmit(event) {
+        var that = this;
+        fetch('/getdata',{
+            method: 'POST',
+            body: JSON.stringify({
+                corporate: that.state.corporate
+            }),
+            headers: {"Content-Type": "application/json"}
+        }).then(function (data) {
+            return data.json()
+        }).then(function(results) {
+            console.log("results",results);
+            that.setState({
+                name: results.name,
+                employees: results.employees,
+                logo: document.getElementById("logoCorporate").src = 'logo'+that.state.corporate+'.png'
+            });
+        });
+        event.preventDefault();
     }
     render() {
         return (
@@ -12,6 +38,16 @@ class Corporate extends React.Component {
                         <div className="mui-col-md-8 mui-col-md-offset-2">
                             <div className="mui--appbar-height"></div>
                             <h1>Corporate</h1>
+                                <form onSubmit={this.handleSubmit}>
+                                    <div className="form-group" >
+                                        <label>Corporate name</label>
+                                        <input type="text" id="corporate" name="corporate" placeholder="orangina-schweppes" value={this.state.corporate} onChange={this.handleChange}/>
+                                    </div>
+                                    <input type="submit" className="btn btn-primary" id="valider" value="Submit"/>
+                                </form>
+                            <p> {this.state.name}</p>
+                            <p> {this.state.employees}</p>
+                            <img src="../src/img/logo.png" id="logoCorporate" alt="logo corporate"/>
                         </div>
                     </div>
                 </div>
--- server.js
+++ server.js
@@ -1,37 +1,54 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var puppeteer = require('puppeteer');
+var fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.static('public/src'));
 app.use(express.static('public/src/img'));
 
-const getData = async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('http://fr.viadeo.com/fr/company/orangina-schweppes');
-    await page.waitFor(1000);
-    const result = await page.evaluate(() => {
-        let name = document.querySelector('h1').innerText;
-        let employees = document.querySelector(".element-value.gu.gu-last").innerText;
-        return {name, employees}
-    });
-    await page.waitForSelector('img');
-    const svgImage = await page.$('img');
-    await svgImage.screenshot({
-        path: 'public/src/img/logo.png',
-        omitBackground: true,
-    });
-    browser.close();
-    return result
-};
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (req, res) {
-    getData().then(value => {
-        console.log(value)
-    });
     res.render('index');
+});
+
+app.post('/getdata', function (req, res) {
+    console.log("req.body.name",  req.body.corporate);
+    const getData = async () => {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('http://fr.viadeo.com/fr/company/'+ req.body.corporate);
+        await page.waitFor(1000);
+        const result = await page.evaluate(() => {
+            let name = document.querySelector('h1').innerText;
+            let employees = document.querySelector(".element-value.gu.gu-last").innerText;
+            return {name, employees}
+        });+
+        await page.waitForSelector('img');
+        const svgImage = await page.$('img');
+        await svgImage.screenshot({
+            path: 'public/src/img/logo'+req.body.corporate+'.png',
+            omitBackground: true,
+        });
+        browser.close();
+        return result
+    };
+    getData().then(value => {
+        const corporate = JSON.stringify(value);
+        console.log("value", value);
+        fs.writeFileSync('public/corporate.json', corporate, 'utf8', function (err){
+            if (err){
+                console.log(err);
+            }
+        });
+        return res.send(value);
+    });
 });
 
 var port= (process.env.PORT || 8080);
